024c8f39885684298bb1f67d411fe99db49259d5
Saurav Shrestha <sauravshrestha28@gmail.com>
2022-01-27 16:36:47 +0545

    Added simple UI

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,23 @@
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.getElementById('messageOne')
+const messageTwo = document.getElementById('messageTwo')
 
 weatherForm.addEventListener('submit', (event) => {
 	event.preventDefault()
 
+	messageOne.textContent = 'Loading...'
+	messageTwo.textContent = ''
 	const location = search.value
 
 	fetch('http://127.0.0.1:3000/weather?address=' + location).then(
 		(response) => {
 			response.json().then((data) => {
 				if (data.error) {
-					console.log(data.error)
+					messageOne.textContent = data.error
 				} else {
-					console.log(data.location)
-					console.log(data.forecast)
+					messageOne.textContent = data.location
+					messageTwo.textContent = data.forecast
 				}
 			})
 		}
